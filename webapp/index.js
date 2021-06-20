4fca39e0fae4f80f6f4220b29d02a4ee0e2b5d0a
Nikhil <nikhilmishra262@gmail.com>
2021-06-20 19:18:35 +0530

    Step 4: Control

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-
-], function() {
+    'sap/m/Text'
+], function(Text) {
     'use strict';
-    alert("UI5 is ready to go!!");
+    new Text({
+        text: 'hello from constructor'
+    }).placeAt("content")
 });
